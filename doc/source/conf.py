9c2bc2fc2bbc2e081c9084d7e0fd3da57ace4f33
Simon Perkins <simon.perkins@gmail.com>
2025-03-24 11:50:26 +0200

    Bump to 0.2.5

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -11,7 +11,7 @@
 project = "xarray-ms"
 copyright = "2024, Simon Perkins"
 author = "Simon Perkins"
-release = "0.2.4"
+release = "0.2.5"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
