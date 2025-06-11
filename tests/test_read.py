Fix low-resolution broadcasting

tests/test_read.py
@@ -273,7 +273,7 @@ def _remove_weight_spectrum_add_weight(chunk_desc, data_dict):
   indirect=True,
 )
 def test_low_resolution_read(simmed_ms):
-  """Test that a missing WEIGHT_SPECTRUM results in a broadcasted WEGITH column"""
+  """Test that a missing WEIGHT_SPECTRUM results in a broadcasted WEIGHT column"""
   dt = xarray.open_datatree(simmed_ms, auto_corrs=True)
 
   for p in ["000", "001"]:
@@ -287,3 +287,51 @@ def test_low_resolution_read(simmed_ms):
     assert_array_equal(
       np.broadcast_to(values, (ntime, nbl, nfreq, npol)), node.WEIGHT.values
     )
+
+
+@pytest.mark.parametrize(
+  "simmed_ms",
+  [
+    {
+      "name": "backend.ms",
+      "nantenna": 7,
+      "data_description": [(8, ["XX", "XY", "YX", "YY"]), (4, ["RR", "LL"])],
+      "table_desc": {k: v for k, v in STANDARD_DATA_COLUMNS.items() if k in {"DATA"}},
+      "transform_data": _remove_weight_spectrum_add_weight,
+    }
+  ],
+  indirect=True,
+)
+def test_isel_loads_on_subsets(simmed_ms):
+  """Test that isels on a broadcasted WEIGHT column"""
+  dt = xarray.open_datatree(simmed_ms, auto_corrs=True)
+  assert len(dt.children) == 2
+
+  p0_sel = {"time": slice(1, 2), "baseline_id": [0, 2], "frequency": slice(4, 6)}
+  # Second partition only has 4 frequencies, tests slicing outside the range
+  p1_sel = {"time": [0, 2], "baseline_id": slice(2, 5), "frequency": slice(4, 6)}
+
+  node0 = dt["backend_partition_000"].isel(**p0_sel)
+  node0.load()
+  assert node0.sizes == {
+    "time": 1,
+    "baseline_id": 2,
+    "frequency": 2,
+    "polarization": 4,
+    "uvw_label": 3,
+  }
+
+  node1 = dt["backend_partition_001"].isel(**p1_sel)
+  node1.load()
+  assert node1.VISIBILITY.shape == (2, 3, 0, 2)
+  assert node1.sizes == {
+    "time": 2,
+    "baseline_id": 3,
+    "frequency": 0,
+    "polarization": 2,
+    "uvw_label": 3,
+  }
+
+  dt.load()
+  assert dt["backend_partition_000"].isel(**p0_sel).identical(node0)
+  assert dt["backend_partition_001"].isel(**p1_sel).identical(node1)

xarray_ms/backend/msv2/array.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from functools import reduce
+from operator import mul
 from typing import TYPE_CHECKING, Any, Callable, Tuple
 
 import numpy as np
@@ -103,6 +105,8 @@ class MainMSv2Array(MSv2Array):
   def _getitem(self, key) -> npt.NDArray:
     assert len(key) == len(self.shape)
     expected_shape = tuple(slice_length(k, s) for k, s in zip(key, self.shape))
+    if reduce(mul, expected_shape, 1) == 0:
+      return np.empty(expected_shape, dtype=self.dtype)
     # Map the (time, baseline_id) coordinates onto row indices
     rows = self._structure_factory.instance[self._partition].row_map[key[:2]]
     row_key = (rows.ravel(),) + key[2:]
@@ -139,13 +143,13 @@ class BroadcastMSv2Array(MSv2Array):
   __slots__ = ("_low_res_array", "_low_res_index")
 
   _low_res_array: MSv2Array
-  _low_res_index: Tuple[slice | None, ...]
+  _low_res_index: Tuple[slice | npt.NDArray | None, ...]
   shape: Tuple[int, ...]
 
   def __init__(
     self,
     low_res_array: MSv2Array,
-    low_res_index: Tuple[slice | None, ...],
+    low_res_index: Tuple[slice | npt.NDArray | None, ...],
     high_res_shape: Tuple[int, ...],
   ):
     self._low_res_array = low_res_array
@@ -165,6 +169,15 @@ class BroadcastMSv2Array(MSv2Array):
     self._low_res_array.transform = value
 
   def __getitem__(self, key) -> npt.NDArray:
-    low_res_data = self._low_res_array.__getitem__(key)
+    return explicit_indexing_adapter(
+      key, self.shape, IndexingSupport.OUTER, self._getitem
+    )
+
+  def _getitem(self, key) -> npt.NDArray:
+    expected_shape = tuple(slice_length(k, s) for k, s in zip(key, self.shape))
+    if reduce(mul, expected_shape, 1) == 0:
+      return np.empty(expected_shape, dtype=self.dtype)
+    low_res_key = tuple(k for i, k in zip(self._low_res_index, key) if i is not None)
+    low_res_data = self._low_res_array._getitem(low_res_key)
     low_res_data = low_res_data[self._low_res_index]
-    return np.broadcast_to(low_res_data, self.shape)
+    return np.broadcast_to(low_res_data, expected_shape)