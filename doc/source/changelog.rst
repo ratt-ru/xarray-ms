c109126597618dfef0c72c213553c57c22bfa06d
Simon Perkins <simon.perkins@gmail.com>
2026-03-06 10:54:42 +0200

    Fix MSv2BroadcastArray pickling error (#147)

--- doc/source/changelog.rst
+++ doc/source/changelog.rst
@@ -5,6 +5,8 @@ Changelog
 
 X.Y.Z (DD-MM-YYYY)
 ------------------
+* Correct ``BroadcastMSv2Array`` initialisation of ``MSv2Array`` base class
+  which resulted in unpickleable ``BroadcastMSv2Array`` instances. (:pr:`147`)
 * Depend on xradio to test xarray-ms MSv4 Datatree compliance (:pr:`148`)
 
 0.5.0 (02-03-2026)
--- xarray_ms/backend/msv2/array.py
+++ xarray_ms/backend/msv2/array.py
@@ -160,7 +160,6 @@ class BroadcastMSv2Array(MSv2Array):
 
   _low_res_array: MSv2Array
   _low_res_index: Tuple[slice | npt.NDArray | None, ...]
-  shape: Tuple[int, ...]
 
   def __init__(
     self,
@@ -168,13 +167,9 @@ class BroadcastMSv2Array(MSv2Array):
     low_res_index: Tuple[slice | npt.NDArray | None, ...],
     high_res_shape: Tuple[int, ...],
   ):
+    super().__init__(high_res_shape, low_res_array.dtype)
     self._low_res_array = low_res_array
     self._low_res_index = low_res_index
-    self.shape = high_res_shape
-
-  @property
-  def dtype(self):
-    return self._low_res_array.dtype
 
   @property
   def transform(self) -> TransformerT | None:
